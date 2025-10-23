865c234aec99f7d9d278260a89e70b4a06d87026
jungseok-corine <nonehour@naver.com>
2025-10-23 10:47:49 +0900

    Initial commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by 오정석 on 23/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
